26b22e6d62887f26ad1719c5598403f839474bea
Dmitry Marov <d.marov94@gmail.com>
2019-03-28 19:49:39 +0300

    client added

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+use std::{ env, thread, time };
+
+fn main() -> std::io::Result<()> {
+
+
+    let args: Vec<String> = env::args()
+        .collect();
+
+    let mut bind_to = String::from("127.0.0.1:8081");
+
+    for i in 0..args.len() {
+
+        if String::from("--bind-to") == args[i] {
+            bind_to = (args[i + 1]).clone();
+        }
+    }
+
+    let mut buf = [0; 10];
+
+    let socket = UdpSocket::bind(bind_to)
+        .unwrap();
+
+    let sleep_duration = time::Duration::from_millis(1000);
+
+    loop {
+
+        socket.recv(&mut buf)
+            .unwrap();
+
+        println!("received {:?}", buf);
+        thread::sleep(sleep_duration);
+    }
+
+    Ok(())
 }
