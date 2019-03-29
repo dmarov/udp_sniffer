f9604513cd1363e29408ec9557855b258d2972bb
Dmitry Marov <d.marov94@gmail.com>
2019-03-29 20:33:31 +0300

    sniffer fix

--- sniffer/src/main.rs
+++ sniffer/src/main.rs
@@ -3,7 +3,7 @@ extern crate pnet;
 use pcap::{ Capture, Device };
 
 use std::env;
-use pnet::packet::{ Packet, udp::UdpPacket };
+use pnet::packet::{ Packet, udp::UdpPacket, ethernet::EthernetPacket, ipv4::Ipv4Packet };
 
 fn main() -> std::io::Result<()> {
 
@@ -41,15 +41,31 @@ fn main() -> std::io::Result<()> {
     cap.filter(filter.as_str())
         .unwrap();
 
-    while let Ok(packet) = cap.next() {
+    loop {
+        while let Ok(packet) = cap.next() {
 
-        let vec = packet.to_vec();
-        let slice = vec.as_slice();
-        let udp_pdu = UdpPacket::new(slice).unwrap();
-        let payload = udp_pdu.payload();
-        let content = &payload[24..];
+            let vec = packet.to_vec();
+            let slice = vec.as_slice();
+            let ethernet_pdu = EthernetPacket::new(slice).unwrap();
+            let ethernet_payload = ethernet_pdu.payload();
+            let ipv4_pdu = Ipv4Packet::new(ethernet_payload).unwrap();
+            let ipv4_payload = ipv4_pdu.payload();
+            let udp_pdu = UdpPacket::new(ipv4_payload).unwrap();
+            let udp_payload = udp_pdu.payload();
 
-        println!("sniffed {:?}", content);
+            println!("{:?}", udp_pdu.get_source());
+            println!("{:?}", udp_pdu.get_destination());
+            println!("{:?}", udp_payload);
+
+            let s = String::from_utf8_lossy(udp_payload);
+            println!("{:?}", s);
+
+
+            // println!("{:?}", udp_pdu.get_length());
+            // println!("sniffed ethernet {:?}", ethernet_payload);
+            // println!("sniffed udp {:?}", udp_payload);
+            // println!("sniffed {:?}", content);
+        }
     }
 
     Ok(())
